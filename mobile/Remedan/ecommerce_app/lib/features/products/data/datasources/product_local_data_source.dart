7b6b9b0b6363a211f7541856a062f70a9756a4f7
Ramsi97 <ramadan2jemal@gmail.com>
2025-08-04 17:26:02 +0300

    task 14 is completed

--- mobile/Remedan/ecommerce_app/lib/features/products/data/datasources/product_local_data_source.dart
+++ mobile/Remedan/ecommerce_app/lib/features/products/data/datasources/product_local_data_source.dart
@@ -1,3 +1,8 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../core/error/exception.dart';
 import '../models/product_model.dart';
 
 abstract class ProductLocalDataSource {
@@ -11,4 +16,46 @@ abstract class ProductLocalDataSource {
   Future<void> cacheProducts(List<ProductModel> products);
 
   Future<ProductModel?> getProductById(String id);
+}
+
+const CACHED_PRODUCTS = 'CACHED_PRODUCTS';
+
+class ProductLocalDataSourceImpl implements ProductLocalDataSource {
+  final SharedPreferences sharedPreferences;
+  ProductLocalDataSourceImpl(this.sharedPreferences);
+
+  @override
+  Future<List<ProductModel>> getCachedProducts() {
+    final jsonString = sharedPreferences.getString(CACHED_PRODUCTS);
+    if (jsonString != null) {
+      final List decoded = json.decode(jsonString);
+      final products = decoded.map((e) => ProductModel.fromJson(e)).toList();
+      return Future.value(products);
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheProducts(List<ProductModel> products) {
+    sharedPreferences.setString(
+      CACHED_PRODUCTS,
+      json.encode(products.map((e) => e.toJson()).toList()),
+    );
+    return Future.value();
+  }
+
+  @override
+  Future<ProductModel?> getProductById(String id) {
+    final products = sharedPreferences.getString(CACHED_PRODUCTS);
+    if (products != null) {
+      final List decoded = json.decode(products);
+      final product = decoded
+          .map((e) => ProductModel.fromJson(e))
+          .firstWhere((p) => p.id == id, orElse: () => null);
+      return Future.value(product);
+    } else {
+      throw CacheException();
+    }
+  }
 }
---
+++ mobile/Remedan/ecommerce_app/test/features/products/data/datasources/product_local_data_source_test.dart
@@ -0,0 +1,140 @@
+import 'dart:convert';
+
+import 'package:ecommerce_app/core/error/exception.dart';
+import 'package:ecommerce_app/features/products/data/datasources/product_local_data_source.dart';
+import 'package:ecommerce_app/features/products/data/models/product_model.dart';
+import 'package:ecommerce_app/features/products/domain/entities/product.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../../fixture/fixture.dart';
+
+class MockShredPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  late ProductLocalDataSourceImpl localDataSource;
+  late MockShredPreferences mockSharedPreferences;
+
+  setUp(() {
+    mockSharedPreferences = MockShredPreferences();
+    localDataSource = ProductLocalDataSourceImpl(mockSharedPreferences);
+  });
+
+  final tProductModel = ProductModel.fromJson(
+    json.decode(fixture('product.json')),
+  );
+  final tProductModels = [tProductModel];
+
+  group('getCachedProduct', () {
+    test('should return a chached data when there is one', () async {
+      // Arrange
+
+      final tProductModels = [tProductModel];
+
+      when(
+        mockSharedPreferences.getString(CACHED_PRODUCTS),
+      ).thenReturn(json.encode(tProductModels.map((e) => e.toJson()).toList()));
+
+      // Act
+      final result = await localDataSource.getCachedProducts();
+
+      // Assert
+      expect(result, [tProductModel]);
+      verify(mockSharedPreferences.getString(CACHED_PRODUCTS));
+    });
+
+    test(
+      'should throw a CacheException when there is no cached data',
+      () async {
+        // Arrange
+        when(mockSharedPreferences.getString(CACHED_PRODUCTS)).thenReturn(null);
+
+        // Act & Assert
+        await expectLater(
+          () => localDataSource.getCachedProducts(),
+          throwsA(isA<CacheException>()),
+        );
+
+        verify(mockSharedPreferences.getString(CACHED_PRODUCTS));
+      },
+    );
+  });
+
+  group('cacheProducts', () {
+    test('should call SharedPreferences to cache the products', () async {
+      // Arrange
+      final tProductModels = [tProductModel];
+      final expectedJsonString = json.encode(
+        tProductModels.map((e) => e.toJson()).toList(),
+      );
+      when(
+        mockSharedPreferences.setString(
+          tProductModels as String,
+          expectedJsonString,
+        ),
+      ).thenAnswer((_) async => true); // or just `=> true;` if method is sync
+
+      // Act
+      await localDataSource.cacheProducts(tProductModels);
+
+      // Assert
+      verify(
+        mockSharedPreferences.setString(CACHED_PRODUCTS, expectedJsonString),
+      );
+    });
+  });
+
+  group('getProductById', () {
+    test('should return product when ID exists in cached data', () async {
+      // Arrange
+      final cachedJsonString = json.encode(
+        tProductModels.map((e) => e.toJson()).toList(),
+      );
+
+      when(
+        mockSharedPreferences.getString(CACHED_PRODUCTS),
+      ).thenReturn(cachedJsonString);
+
+      // Act
+      final result = await localDataSource.getProductById(tProductModel.id);
+
+      // Assert
+      expect(result, equals(tProductModel));
+      verify(mockSharedPreferences.getString(CACHED_PRODUCTS));
+    });
+
+    test(
+      'should throw CacheException when no product is found for ID',
+      () async {
+        // Arrange
+        final cachedJsonString = json.encode(
+          tProductModels.map((e) => e.toJson()).toList(),
+        );
+
+        when(
+          mockSharedPreferences.getString(CACHED_PRODUCTS),
+        ).thenReturn(cachedJsonString);
+
+        // Act
+        final call = localDataSource.getProductById('non_existing_id');
+
+        // Assert
+        await expectLater(() => call, throwsA(isA<CacheException>()));
+        verify(mockSharedPreferences.getString(CACHED_PRODUCTS));
+      },
+    );
+
+    test('should throw CacheException when no cache exists', () async {
+      // Arrange
+      when(mockSharedPreferences.getString(CACHED_PRODUCTS)).thenReturn(null);
+
+      // Act
+      final call = localDataSource.getProductById(tProductModel.id);
+
+      // Assert
+      await expectLater(() => call, throwsA(isA<CacheException>()));
+      verify(mockSharedPreferences.getString(CACHED_PRODUCTS));
+    });
+  });
+}
